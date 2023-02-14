05e06221356d548702760ed21ba78ce59f2d9e21
sazid <mdip@uxd.co.jp>
2023-02-14 17:45:56 +0900

    Initial Commit

---
+++ gitBee/gitBeeApp.swift
@@ -0,0 +1,20 @@
+//
+//  gitBeeApp.swift
+//  gitBee
+//
+//  Created by MD SAZID HASAN DIP on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct gitBeeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
